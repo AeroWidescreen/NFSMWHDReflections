7caf55c63aef314f077d09055246ebc28752a692
Aero_ <19331642+AeroWidescreen@users.noreply.github.com>
2019-02-26 19:29:57 -0800

    Initial commit

---
+++ NFSMWHDReflections/dllmain.cpp
@@ -0,0 +1,102 @@
+﻿#include "stdafx.h"
+#include "stdio.h"
+#include <windows.h>
+#include "..\includes\injector\injector.hpp"
+#include <cstdint>
+#include "..\includes\IniReader.h"
+#include <d3d9.h>
+
+DWORD WINAPI Thing(LPVOID);
+
+bool HDReflections, ImproveReflectionLOD, GeometryFix;
+static int ResolutionX, ResolutionY;
+DWORD GameState;
+HWND windowHandle;
+
+DWORD RestoreFEReflectionCodeCaveExit = 0x6BD502;
+DWORD VehicleReflectionCodeCaveExit = 0x6BD533;
+
+void __declspec(naked) RestoreFEReflectionCodeCave()
+{
+	__asm {
+		mov ecx, 0x100
+		jmp RestoreFEReflectionCodeCaveExit
+	}
+}
+
+void __declspec(naked) VehicleReflectionCodeCave()
+{
+	__asm {
+		mov edi, dword ptr ds : [0x8F8FF4]
+		mov edx, dword ptr ds : [0x8F8FF8]
+		jmp VehicleReflectionCodeCaveExit
+	}
+}
+
+void Init()
+{
+	// Read values from .ini
+	CIniReader iniReader("NFSMWHDReflections.ini");
+
+	// Resolution
+	ResolutionX = iniReader.ReadInteger("RESOLUTION", "ResolutionX", 1920);
+	ResolutionY = iniReader.ReadInteger("RESOLUTION", "ResolutionY", 1080);
+
+	// General
+	HDReflections = iniReader.ReadInteger("GENERAL", "HDReflections", 1);
+	ImproveReflectionLOD = iniReader.ReadInteger("GENERAL", "ImproveReflectionLOD", 1);
+	GeometryFix = iniReader.ReadInteger("GENERAL", "GeometryFix", 1);
+
+
+	if (HDReflections)
+	{
+		// Jumps
+		injector::MakeJMP(0x6BD4FC, RestoreFEReflectionCodeCave, true);
+		injector::MakeJMP(0x6BD52D, VehicleReflectionCodeCave, true);
+		// Road Reflection X
+		injector::WriteMemory<uint32_t>(0x6CFC26, ResolutionX, true);
+		injector::WriteMemory<uint32_t>(0x6BCDF5, ResolutionX, true);
+		injector::WriteMemory<uint32_t>(0x6BD17D, ResolutionX, true);
+		// Road Reflection Y
+		injector::WriteMemory<uint32_t>(0x6BD184, ResolutionY, true);
+		injector::WriteMemory<uint32_t>(0x6CFC2D, ResolutionY, true);
+		injector::WriteMemory<uint32_t>(0x6BCDEF, ResolutionY, true);
+		// Vehicle Reflection
+		injector::WriteMemory<uint32_t>(0x8F8FF4, ResolutionY, true);
+		// RVM Reflection
+		injector::WriteMemory<uint32_t>(0x8F9008, ResolutionY, true);
+		injector::WriteMemory<uint32_t>(0x8F900C, ResolutionY, true);
+	}
+
+	if (ImproveReflectionLOD)
+	{
+		injector::WriteMemory<uint8_t>(0x4FAEB0, 0xEB, true);
+	}
+
+	if (GeometryFix)
+	{
+		injector::WriteMemory<uint8_t>(0x6C69AE, 0xEB, true);
+	}
+}
+	
+
+BOOL APIENTRY DllMain(HMODULE /*hModule*/, DWORD reason, LPVOID /*lpReserved*/)
+{
+	if (reason == DLL_PROCESS_ATTACH)
+	{
+		uintptr_t base = (uintptr_t)GetModuleHandleA(NULL);
+		IMAGE_DOS_HEADER* dos = (IMAGE_DOS_HEADER*)(base);
+		IMAGE_NT_HEADERS* nt = (IMAGE_NT_HEADERS*)(base + dos->e_lfanew);
+
+		if ((base + nt->OptionalHeader.AddressOfEntryPoint + (0x400000 - base)) == 0x7C4040) // Check if .exe file is compatible - Thanks to thelink2012 and MWisBest
+			Init();
+
+		else
+		{
+			MessageBoxA(NULL, "This .exe is not supported.\nPlease use v1.3 English speed.exe (5,75 MB (6.029.312 bytes)).", "NFSMW HD Reflections", MB_ICONERROR);
+			return FALSE;
+		}
+	}
+	return TRUE;
+	
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# NFSMW HD Reflections
+
+## Features
+- ResolutionX : Controls the horizontal resolution of reflections.  
+- ResolutionY : Controls the vertical resolution of reflections. 
+- HDReflections : Reflection resolution will be increased based on the values set.  
+- ImproveReflectionLOD : Increases the level of detail seen in the rearview mirror, but may cause objects to disappear.  
+- GeometryFix : Restores missing geometry, such as frontend reflections and world detail.
+
+## Credits
+[Aero_](https://github.com/AeroWidescreen) : Creator of NFSMW HD Reflections  
+[ExOpts Team](https://github.com/ExOptsTeam/) : The majority of the project is based on their work. This wouldn't exist without them.  
